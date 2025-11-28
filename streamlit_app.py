90e56a0a3b0b195537f432e214a8ed3472fa8544
BenLoos11 <Ben.Loos@trojans.dsu.edu>
2025-11-28 14:46:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 
